e80d135f1244fce1307872d662df1d547d6e46e8
Atharva-Iparkar <iparkaratharva@gmail.com>
2025-01-23 11:59:07 +0530

    Update p1.cpp

--- DSA/p1.cpp
+++ DSA/p1.cpp
@@ -1,121 +1,194 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 using namespace std;
 
-class node {
-public: 
-string name;
-int id;
-long int tel;
-friend class hashing;
+class set {
+	public:
+		int size1, size2;
+		vector<int> set1, set2;
+		
+	void insert() {
+		cout<<"Enter the size of set 1 : ";
+		cin>>size1;
+		cout<<"Enter the elements of set 1 "<<endl;
+		for(int i=0; i<size1; ++i) {
+			int val;
+			cin>>val;
+			set1.push_back(val);
+		}
+		
+		cout<<"Enter the size of set 2 : ";
+		cin>>size2;
+		cout<<"Enter the elements of set 2 "<<endl;
+		for(int i=0; i<size2; ++i) {
+			int val;
+			cin>>val;
+			set2.push_back(val);
+		}
+	}
+	
+	void display() {
+		cout<<"\nThe elements of set 1 are : ";
+		for(int i=0; i<size1; i++) {
+			cout<<set1[i]<<" ";
+		}
+		
+		cout<<"\nThe elements of set 2 are : ";
+		for(int i=0; i<size2; i++) {
+			cout<<set2[i]<<" ";
+		}
+	}
+	
+	void union_() {
+		int k = 0;
+		vector<int> result;
+		for(int i=0; i<size1; ++i) {
+			result.push_back(set1[i]);
+			k++;
+		}
+		
+		for(int i=0; i<size2; ++i) {
+			bool f = false;
+			for(int j=0; j<size1; ++j) {
+				if(set2[i] == set1[j]) {
+					f = true;
+				}
+			}
+			if(f == false) {
+				result.push_back(set2[i]);
+				k++;
+			}
+		}
+		
+		cout<<"\nUnion of the sets is : ";
+		for(int i=0; i<k; i++) {
+			cout<<result[i]<<" ";
+		}
+	}
+	
+	void intersection() {
+		int k = 0;
+		vector<int> result;
+		for(int i=0; i<size1; ++i) {
+			for(int j=0; j<size2; ++j) {
+				if(set1[i] == set2[j]) {
+					result.push_back(set1[i]);
+					k++;
+				}
+			}
+		}
+		
+		cout<<"\nInsersection of the sets is : ";
+		for(int i=0; i<k; i++) {
+			cout<<result[i]<<" ";
+		}
+	}
+	
+	void subset() {
+		int k = 0;
+		for(int i=0; i<size2; ++i) {
+			for(int j=0; j<size1; ++j) {
+				if(set2[i] == set1[j]) {
+					k++;
+				}
+			}
+		}
+		
+		if(k == set2.size()) {
+			cout<<"\nSet 2 is a subset of set 1";
+		} else {
+			cout<<"\nSet 2 is not a subset of set 1";
+		}
+	}
+	
+	void difference() {
+		vector<int> result;
+		int k = 0;
+		for(int i=0; i<size1; ++i) {
+			bool f = false;
+			for(int j=0; j<size2; ++j) {
+				if(set1[i] == set2[j]) {
+					f = true;
+				}
+			}
+			if(!f) {
+				k++;
+				result.push_back(set1[i]);
+			}
+		}
+		
+		cout<<"\nDifference of set 1 & set 2 is : ";
+		for(int i=0; i<k; i++) {
+			cout<<result[i]<<" ";
+		}
+	}
+	
+	void remove_element() {
+		int val;
+		int ch;
+		bool f = false;
+		cout<<"From which you want to remove element Set 1 or Set 2 : ";
+		cin>>ch;
+		if(ch == 1) {
+			cout<<"Enter value to be removed : ";
+			cin>>val;
+			for(int i=0; i<size1; i++) {
+				if(set1[i] == val) {
+					set1.erase(set1.begin() + i);
+					f = true;
+					break;
+				}
+			}
+		} else if(ch == 2) {
+			cout<<"Enter value to be removed : ";
+			cin>>val; 
+			for(int i=0; i<size2; i++) {
+				if(set2[i] == val) {
+					set2.erase(set2.begin() + i);
+					f = true;
+					break;
+				}
+			}	
+		}
 
-node() {
-name = "";
-id = 0;
-tel = 0;
-}
-};
-
-class hashing {
-public:
-node data[10];
-int index;
-int size = 10;
-
-void create_Record(string n, int i, int tel) {
-index = i % size;
-for(int j=0; j<size; j++) {
-if(data[index].id == 0) {
-data[index].name = n;
-data[index].id = i;
-data[index].tel = tel;
-break; 
-} else {
-index = (index+1) % size;
-}
-}
-}
-
-void getdata_Create() {
-string name;
-int id;
-int tele;
-cout<<"Enter name : ";
-cin>>name;
-cout<<"Enter id : ";
-cin>>id;
-cout<<"Enter telephone no. : ";
-cin>>tele;
-create_Record(name, id, tele);
-}
-
-void display_Record() {
-for(int j=0; j<size; j++) {
-if(data[j].name != "") {
-cout<<"Name : "<<data[j].name<<endl;
-cout<<"Id : "<<data[j].id<<endl;
-cout<<"Telephone no. : "<<data[j].tel<<endl;
-}
-}
-}
-
-void search_Record() {
-int i;
-cout<<"Enter id of record want to search : ";
-cin>>i;
-for(int j=0; j<size; j++) {
-if(data[i].id == data[j].id) {
-cout<<"Name : "<<data[j].name<<endl;
-cout<<"Id : "<<data[j].id<<endl;
-cout<<"Telephone no. : "<<data[j].tel<<endl;
-break;
-}
-}
-}
-
-void delete_Record() {
-int i;
-int flag = 0;
-cout<<"Enter id of record want to delete : ";
-cin>>i;
-index = i % size;
-for(int j=0; j<size; j++) {
-if(i == data[index].id) {
-flag = 1;
-data[index].id = 0;
-data[index].name = "";
-data[index].tel = 0;
-} else {
-index = (index+1) % size;
-}
-}
-}
+		if(!f) {
+			cout<<"Value not found in the set"<<endl;
+		} else {
+			cout<<"Value "<<val<<" has been removed"<<endl;
+		}
+	}
 };
 
 int main() {
-hashing h;
-
-int ch;
-do {
-cout<<" 1. Get data\n 2. Delete\n 3. Display\n 4. Search\n 5. Exit\n";
-cout<<"Enter your choice : ";
-cin>>ch;
-
-switch(ch) {
-case 1:
-h.getdata_Create();
-break;
-case 2:
-h.delete_Record();
-break;
-case 3:
-h.display_Record();
-break;
-case 4:
-h.search_Record();
-break;
-case 5:
-return 0;
-}
-} 
-while(ch);
+	set s;
+	int ch;
+	s.insert();
+	do {
+		cout<<"\n\n1. Union \n2.Intersection \n3. Subset \n4. Difference \n5. Remove Element \n6. Display \n7. Exit \nEnter your choice : ";
+		cin>>ch;
+		
+		switch(ch) {
+			case 1:
+				s.union_();
+				break;
+			case 2:
+				s.intersection();
+				break;
+			case 3:
+				s.subset();
+				break;
+			case 4:
+				s.difference();
+				break;
+			case 5:
+				s.remove_element();
+				break;
+			case 6:
+				s.display();
+				break;
+			case 7:
+				return 0;
+		}
+	}	 
+	while(ch);
 }
