4a954448c473ea73bdda0eef15829992ad2866d8
Atharva-Iparkar <iparkaratharva@gmail.com>
2025-01-30 10:48:57 +0530

    Update p4.cpp

--- DSA/p4.cpp
+++ DSA/p4.cpp
@@ -62,3 +62,111 @@ int main() {
         }
     } while (true);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+
+
+struct node {
+    int info;
+    struct node* left;
+    struct node* right;
+
+    node(int val) {
+        info = val;
+        left = right = NULL;
+    }
+};
+
+class BST {
+public:
+    node* root;
+
+    BST() {
+        root = NULL;
+    }
+
+   
+    void insert(node* &tree, int value) {
+        if (tree == NULL) {
+            tree = new node(value);  
+            cout << "Node with value " << value << " is added!" << endl;
+        }
+        else if (value < tree->info) {
+            insert(tree->left, value);  
+        }
+        else if (value > tree->info) {
+            insert(tree->right, value);  
+        }
+        
+    }
+
+    
+    void inorder(node* tree) {
+        if (tree == NULL) return;
+        inorder(tree->left);
+        cout << tree->info << " ";
+        inorder(tree->right);
+    }
+
+   
+    void inorder() {
+        inorder(root);
+    }
+};
+
+
+int main() {
+    BST tree;
+
+    
+    tree.insert(tree.root, 50);
+    tree.insert(tree.root, 30);
+    tree.insert(tree.root, 20);
+    tree.insert(tree.root, 40);
+    tree.insert(tree.root, 70);
+    tree.insert(tree.root, 60);
+    tree.insert(tree.root, 80);
+
+    
+    cout << "Inorder Traversal: ";
+    tree.inorder();
+    cout << endl;
+
+    return 0;
+}
+
